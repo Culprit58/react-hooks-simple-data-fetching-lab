dd78cc6ab0f4d90ebbf6c31c739a628ffb4b2af1
Enock Cheruiyot <cheruiyote518@gmail.com>
2025-10-18 16:18:38 +0300

    lab done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching dog image:", error);
+        setIsLoading(false);
+      });
+  }, []); // runs once when component mounts
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
